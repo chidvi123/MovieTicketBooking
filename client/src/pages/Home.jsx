4bc2f8bcf9fbcc2e31d00de9e34306148c87de9a
Chidvilas Palarpa <palarpachidvilas2419@gmail.com>
2026-02-01 23:28:07 +0530

    background for the website,hero section and feature section and blur circle are created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <HeroSection/>
+    <FeatureSection/>
+    </>
   )
 }
 
