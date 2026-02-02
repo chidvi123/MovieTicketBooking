34d16741aae4296b959805f23aa242f1bf871c60
Chidvilas Palarpa <palarpachidvilas2419@gmail.com>
2026-02-02 15:06:15 +0530

    made small changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeatureSection from '../components/FeatureSection'
-import MovieCard from '../components/MovieCard'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
     <HeroSection/>
     <FeatureSection/>
-
+    <TrailerSection/>
     </>
   )
 }
